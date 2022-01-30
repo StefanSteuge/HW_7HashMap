b57d2c6e4028f6cf08cc6713c2e1f1300746ed31
Stepan Dulka <fotosteuge@gmail.com>
2022-01-30 19:51:08 +0200

    modified main

--- src/com/cursor/Main.java
+++ src/com/cursor/Main.java
@@ -79,33 +79,3 @@ public class Main {
 }
 
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-/*     *  You are a library visitor.
-
-        Every time when you take a book, it should be added to the library report as well.
-
-        The report consists of a date ( when you took a book), the title of the book.
-        For dates use LocalDate
-
-        Implement this structure using Map. Add functionality to search on the map by dates.
-
-        F.e. for 20-10-19 will return the title of the book.
-
-        If there are no books for this date - return message,
-        some things like " There are no books for this date " (or whatever).
-
-
-        Also, return range of dates of taken books and separately list of books for a whole range.
-*/
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-/*        Rewrite functionality so that you could take a few books in one day.
-
-          Return range of dates for ONE month (day by day chronologically) with a count of books.
-
-          If for a particular day there are no taken books, return 0 as value.
-
-          F.e. 01-01-2020 - 2
-
-          02-01-2020 - 0
-*/
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
